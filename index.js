d9ab3e799bb56001b12ff04d7ae54a3ec3f81c86
scrabill <shannon.crabill@gmail.com>
2019-12-21 15:20:44 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+let main = document.querySelector('main')
+main.remove();
+
+let body = document.querySelector('body')
+let newHeader = document.createElement('h1')
+
+// newHeader.addClassName = "victory"
+newHeader.id = "victory"
+newHeader.innerHTML = "Shannon is the champion"
+
+body.appendChild(newHeader)
