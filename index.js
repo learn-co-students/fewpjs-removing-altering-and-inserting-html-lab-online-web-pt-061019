c7add7d94f0029dc68c977bcb437b0992adcf93d
Sam Markham <sam.e.markham@gmail.com>
2020-01-06 18:02:10 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+let main = document.querySelector("main#main");
+main.remove();
+
+let newHeader = document.createElement("h1");
+document.body.appendChild(newHeader);
+
+newHeader.id = "victory";
+newHeader.innerHTML = "YOUR-NAME is the champion";
+
+
+// let newHeader = document.body.appendChild(document.createElement("h1#victory"));
