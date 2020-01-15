79c8043d7e49c9abc9cffd3144daa329449d0130
tshuttles <tshuttlesworth@gmail.com>
2020-01-15 00:56:45 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+document.getElementById('main').remove('main');
+// main.remove()
+
+let newHeader = document.createElement("h1"); 
+newHeader.id = "victory";
+newHeader.innerHTML = "Tachi is the champion"; 
+document.body.appendChild(newHeader); 
