9f41ed09b3329a9d53d526f73c0452de490f3441
Christian_Polanco <chrispolanco@sbcglobal.net>
2020-01-19 20:09:45 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+document.querySelector('main').remove(); 
+
+let newHeader = document.createElement('h1'); 
+document.body.appendChild(newHeader); 
+
+newHeader.id = 'victory'; 
+newHeader.innerHTML = 'Chris is the champion'; 
